e32932ab6987d467bd9508613c4bb1b7beafe3b2
sebashtioon <sebastiansuciu607@gmail.com>
2026-06-17 23:59:01 +1000

    hmmo okay

--- ast/actors/props/phone/phone.gd
+++ ast/actors/props/phone/phone.gd
@@ -16,3 +16,6 @@ func _on_phone_pickup() -> void:
 	
 	phonepickup.play()
 	ringanimation.stop()
+	
+	await get_tree().create_timer(1.0).timeout
+	PlayerGlobal.world.sequencestart.play(&"main")
--- src/main/main.gd
+++ src/main/main.gd
@@ -2,9 +2,11 @@ extends Node3D
 
 @export var mainmenu_camera : Camera3D
 @export var gamemanager : Node3D
-
+@export var gamesequence: Node3D
+@export var sequencestart: AnimationPlayer
 
 
 func _ready() -> void:
 	#mainmenu_camera.make_current()
-	pass
+	
+	PlayerGlobal.world = $"."
