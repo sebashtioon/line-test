691722c8557be9b009155ba5222ca9bf158e60e4
sebashtioon <sebastiansuciu607@gmail.com>
2026-06-17 19:07:35 +1000

    da heel

--- ast/actors/props/phone/phone.gd
+++ ast/actors/props/phone/phone.gd
@@ -1,7 +1,16 @@
 extends Node3D
 
 @export var ringanimation: AnimationPlayer
+@export var phone: MeshInstance3D
 
 
 func _ready() -> void:
 	ringanimation.play(&"main")
+
+
+
+func _on_phone_pickup() -> void:
+	phone.visible = false # set model phone mesh to invis
+	PlayerGlobal.player.phone.visible = true # phone on player head visible
+	
+	ringanimation.stop()
