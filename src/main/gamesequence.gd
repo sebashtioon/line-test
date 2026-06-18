d7ce2bd931ac2ee747c733f18ac5b5782fc875d2
sebashtioon <sebastiansuciu607@gmail.com>
2026-06-19 09:22:43 +1000

    okay niec

--- src/main/gamesequence.gd
+++ src/main/gamesequence.gd
@@ -2,7 +2,15 @@ extends Node3D
 
 @export var door: Node3D
 
+
+# flags
+var door_spawned_start : bool = false
+
 func spawn_door() -> void:
 	door.position.x = PlayerGlobal.player.door_spawn.global_position.x
 	door.position.z = PlayerGlobal.player.door_spawn.global_position.z
 	door.rotation_degrees.y = PlayerGlobal.player.door_spawn.global_rotation_degrees.y - 90
+
+func _on_door_look(area: Area3D) -> void:
+	if area.is_in_group(&"raycast_mimic"):
+		pass
