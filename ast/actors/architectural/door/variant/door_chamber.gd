f5ae79820e83d411cff213374222ac5307870afe
sebashtioon <sebastiansuciu607@gmail.com>
2026-07-08 11:39:28 +1000

    okay pretty much done

--- ast/actors/architectural/door/variant/door_chamber.gd
+++ ast/actors/architectural/door/variant/door_chamber.gd
@@ -11,6 +11,7 @@ var DOOR_STATE : DOOR_STATES = DOOR_STATES.CLOSED
 @export var mesh: MeshInstance3D
 
 @export var key: Label
+@export var ui: Sprite3D
 
 var can_use_door : bool = true
 var can_interact_with_door : bool = true
@@ -40,3 +41,9 @@ func start_debounce() -> void:
 
 func _on_debounce_timeout() -> void:
 	can_use_door = true
+
+
+func _on_door_animation_animation_finished(anim_name: StringName) -> void:
+	if anim_name == &"close" and PlayerGlobal.world.gamesequence.in_chamberdoor_hallway:
+		can_interact_with_door = false
+		ui.visible = false
