1b7bdccf8d8a1d82e72959d0b04036c5927d1b1d
sebashtioon <sebastiansuciu607@gmail.com>
2026-07-09 00:03:49 +1000

    almost

--- src/demo/mainmenu_demo.gd
+++ src/demo/mainmenu_demo.gd
@@ -2,4 +2,8 @@ extends Control
 
 
 func _on_playbtn_pressed() -> void:
-	pass # Replace with function body.
+	$bg.visible = true
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	
+	await get_tree().create_timer(2.0).timeout
+	get_tree().change_scene_to_file("res://src/main/main.tscn")
