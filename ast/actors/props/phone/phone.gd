b936d36f3bf2f44d43d6edacf67cddfbf26c76ef
sebashtioon <sebastiansuciu607@gmail.com>
2026-07-08 12:35:51 +1000

    phone interaction prevention after picking up

--- ast/actors/props/phone/phone.gd
+++ ast/actors/props/phone/phone.gd
@@ -4,17 +4,21 @@ extends Node3D
 @export var phone: MeshInstance3D
 @export var phonepickup: AudioStreamPlayer3D
 
+var phone_pickedup : bool = false
 
 func _ready() -> void:
 	ringanimation.play(&"main")
 
 func _on_phone_pickup() -> void:
-	phone.visible = false # set model phone mesh to invis
-	PlayerGlobal.player.phone.visible = true # phone on player head visible
-	
-	phonepickup.play()
-	ringanimation.stop()
-	
-	await get_tree().create_timer(1.0).timeout
-	PlayerGlobal.world.gamesequence.sequencestart.play(&"sequencestart/main")
-	#PlayerGlobal.world.gamesequence.spawn_door() # TEST (COMMENTED)
+	if !phone_pickedup:
+		phone_pickedup = true
+		phone.visible = false # set model phone mesh to invis
+		PlayerGlobal.player.phone.visible = true # phone on player head visible
+		
+		phonepickup.play()
+		ringanimation.stop()
+		$interactablecomponent/Contents/UI.visible = false
+		
+		await get_tree().create_timer(1.0).timeout
+		PlayerGlobal.world.gamesequence.sequencestart.play(&"sequencestart/main")
+		#PlayerGlobal.world.gamesequence.spawn_door() # TEST (COMMENTED)
